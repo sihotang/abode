edec28d32dd6597bb7d6bf98e587d576f615b606
Sopar Sihotang <soparsihotang@gmail.com>
2018-07-07 21:56:54 +0700

    New installation php by using choco

---
+++ scripts/powershell/install-php.ps1
@@ -0,0 +1,12 @@
+# Install PHP Stuffs
+# Current PHP (7.2)
+choco install php --params '"/NonThreadSafe /DontAddToPath /InstallDir:C:\PHP\php7.2"' --version 7.2.7 -my
+
+# PHP 7.1
+choco install php --params '"/NonThreadSafe /DontAddToPath /InstallDir:C:\PHP\php7.1"' --version 7.1.19 -my
+
+# PHP 7.0
+choco install php --params '"/NonThreadSafe /DontAddToPath /InstallDir:C:\PHP\php7.0"' --version 7.0.30 -my
+
+# PHP 5.6
+choco install php --params '"/NonThreadSafe /DontAddToPath /InstallDir:C:\PHP\php5.6"' --version 5.6.36 -my
