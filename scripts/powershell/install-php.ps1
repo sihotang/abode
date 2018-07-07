bbd3dbe3d77e9b1bf250aea82f85701f83d7fb3b
Sopar Sihotang <soparsihotang@gmail.com>
2018-07-08 06:47:19 +0700

    Update indet style for editor config to space

--- scripts/powershell/install-php.ps1
+++ scripts/powershell/install-php.ps1
@@ -34,56 +34,56 @@ choco install php --params '"/NonThreadSafe /DontAddToPath /InstallDir:C:\PHP\ph
 
 # Set Configuration PHP CGI for IIS
 If ((Get-WindowsOptionalFeature -Online -FeatureName IIS-CGI).State -ne "Disabled") {
-	# Set Fast CGI
+    # Set Fast CGI
     # Get-WebConfigurationProperty -Filter /system.webServer/fastCGI -Name collection | select fullPath
     Add-WebConfigurationProperty /system.webServer/fastCgi -Name collection -Value @{fullPath = "C:\PHP\php7.2\php-cgi.exe"; activityTimeout = 600; instanceMaxRequests = 10000}
     Add-WebConfigurationProperty /system.webServer/fastCgi -Name collection -Value @{fullPath = "C:\PHP\php7.1\php-cgi.exe"; activityTimeout = 600; instanceMaxRequests = 10000}
-	Add-WebConfigurationProperty /system.webServer/fastCgi -Name collection -Value @{fullPath = "C:\PHP\php7.0\php-cgi.exe"; activityTimeout = 600; instanceMaxRequests = 10000}
+    Add-WebConfigurationProperty /system.webServer/fastCgi -Name collection -Value @{fullPath = "C:\PHP\php7.0\php-cgi.exe"; activityTimeout = 600; instanceMaxRequests = 10000}
     Add-WebConfigurationProperty /system.webServer/fastCgi -Name collection -Value @{fullPath = "C:\PHP\php5.6\php-cgi.exe"; activityTimeout = 600; instanceMaxRequests = 10000}
 
-	# Set Handler Mapping based on Fast CGI module
-	New-WebHandler -Name "PHP72_via_FastCGI" -Path "*.php" -Verb "*" -Modules "FastCgiModule" -ScriptProcessor "C:\PHP\php7.2\php-cgi.exe" -ResourceType Either
+    # Set Handler Mapping based on Fast CGI module
+    New-WebHandler -Name "PHP72_via_FastCGI" -Path "*.php" -Verb "*" -Modules "FastCgiModule" -ScriptProcessor "C:\PHP\php7.2\php-cgi.exe" -ResourceType Either
     New-WebHandler -Name "PHP71_via_FastCGI" -Path "*.php" -Verb "*" -Modules "FastCgiModule" -ScriptProcessor "C:\PHP\php7.1\php-cgi.exe" -ResourceType Either
     New-WebHandler -Name "PHP70_via_FastCGI" -Path "*.php" -Verb "*" -Modules "FastCgiModule" -ScriptProcessor "C:\PHP\php7.0\php-cgi.exe" -ResourceType Either
     New-WebHandler -Name "PHP56_via_FastCGI" -Path "*.php" -Verb "*" -Modules "FastCgiModule" -ScriptProcessor "C:\PHP\php5.6\php-cgi.exe" -ResourceType Either
 
-	# Set Default Document (Default.php, index.php)
-	Add-WebConfigurationProperty /system.webServer/defaultDocument -Name files -Value @{ value = "Default.php" }
+    # Set Default Document (Default.php, index.php)
+    Add-WebConfigurationProperty /system.webServer/defaultDocument -Name files -Value @{ value = "Default.php" }
     # Remove-WebConfigurationProperty /system.webServer/defaultDocument -Name files -AtElement @{ value = "Default.php" }
-	Add-WebConfigurationProperty /system.webServer/defaultDocument -Name files -Value @{ value = "index.php" }
-	# Remove-WebConfigurationProperty /system.webServer/defaultDocument -Name files -AtElement @{ value = "index.php" }
+    Add-WebConfigurationProperty /system.webServer/defaultDocument -Name files -Value @{ value = "index.php" }
+    # Remove-WebConfigurationProperty /system.webServer/defaultDocument -Name files -AtElement @{ value = "index.php" }
 
-	# Setup sites with specific php version
-	$Script = "<?php phpinfo(); ?>"
+    # Setup sites with specific php version
+    $Script = "<?php phpinfo(); ?>"
     $PathWWWRoot = (Get-ItemProperty HKLM:\Software\Microsoft\INetStp -Name "PathWWWRoot").PathWWWRoot
 
-	New-Item -Path $PathWWWRoot -Name "phpinfo.php" -ItemType "file" -Value $Script
+    New-Item -Path $PathWWWRoot -Name "phpinfo.php" -ItemType "file" -Value $Script
 
-	# PHP 7.2
+    # PHP 7.2
     New-Item -Path $PathWWWRoot -Name "php72" -ItemType "directory"
-	New-Item -Path ($PathWWWRoot + "\php72") -Name "index.php" -ItemType "file" -Value $Script
+    New-Item -Path ($PathWWWRoot + "\php72") -Name "index.php" -ItemType "file" -Value $Script
     New-WebApplication -Name PHP72 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php72") -ApplicationPool DefaultAppPool
     Get-WebHandler -PSPath 'IIS:\Sites\Default Web Site\PHP72' | Where-Object -FilterScript {($_.Path -eq "*.php") -and ($_.Name -ne "PHP72_via_FastCGI")} | ForEach-Object { Remove-WebHandler -Name $_.Name -PSPath 'IIS:\Sites\Default Web Site\PHP72' }
 
     # PHP 7.1
     New-Item -Path $PathWWWRoot -Name "php71" -ItemType "directory"
-	New-Item -Path ($PathWWWRoot + "\php71") -Name "index.php" -ItemType "file" -Value $Script
-	New-WebApplication -Name PHP71 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php71") -ApplicationPool DefaultAppPool
+    New-Item -Path ($PathWWWRoot + "\php71") -Name "index.php" -ItemType "file" -Value $Script
+    New-WebApplication -Name PHP71 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php71") -ApplicationPool DefaultAppPool
     Get-WebHandler -PSPath 'IIS:\Sites\Default Web Site\PHP71' | Where-Object -FilterScript {($_.Path -eq "*.php") -and ($_.Name -ne "PHP71_via_FastCGI")} | ForEach-Object { Remove-WebHandler -Name $_.Name -PSPath 'IIS:\Sites\Default Web Site\PHP71' }
 
-	# PHP 7.0
+    # PHP 7.0
     New-Item -Path $PathWWWRoot -Name "php70" -ItemType "directory"
-	New-Item -Path ($PathWWWRoot + "\php70") -Name "index.php" -ItemType "file" -Value $Script
-	New-WebApplication -Name PHP70 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php70") -ApplicationPool DefaultAppPool
+    New-Item -Path ($PathWWWRoot + "\php70") -Name "index.php" -ItemType "file" -Value $Script
+    New-WebApplication -Name PHP70 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php70") -ApplicationPool DefaultAppPool
     Get-WebHandler -PSPath 'IIS:\Sites\Default Web Site\PHP70' | Where-Object -FilterScript {($_.Path -eq "*.php") -and ($_.Name -ne "PHP70_via_FastCGI")} | ForEach-Object { Remove-WebHandler -Name $_.Name -PSPath 'IIS:\Sites\Default Web Site\PHP70' }
 
     # PHP 5.6
     New-Item -Path $PathWWWRoot -Name "php56" -ItemType "directory"
-	New-Item -Path ($PathWWWRoot + "\php56") -Name "index.php" -ItemType "file" -Value $Script
-	New-WebApplication -Name PHP56 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php56") -ApplicationPool DefaultAppPool
+    New-Item -Path ($PathWWWRoot + "\php56") -Name "index.php" -ItemType "file" -Value $Script
+    New-WebApplication -Name PHP56 -Site 'Default Web Site' -PhysicalPath ($PathWWWRoot + "\php56") -ApplicationPool DefaultAppPool
     Get-WebHandler -PSPath 'IIS:\Sites\Default Web Site\PHP56' | Where-Object -FilterScript {($_.Path -eq "*.php") -and ($_.Name -ne "PHP56_via_FastCGI")} | ForEach-Object { Remove-WebHandler -Name $_.Name -PSPath 'IIS:\Sites\Default Web Site\PHP56' }
 
     # Restart Services
-	Invoke-Expression -Command "IISRESET"
-	Get-Service W3SVC | Start-Service
+    Invoke-Expression -Command "IISRESET"
+    Get-Service W3SVC | Start-Service
 }
