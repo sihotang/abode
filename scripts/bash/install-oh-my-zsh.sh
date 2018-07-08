c757f16ad1f3786fbbc43e6c5824d166a5d3b416
Sopar Sihotang <soparsihotang@gmail.com>
2018-07-08 19:11:48 +0700

    New installation script for oh-my-zsh

---
+++ scripts/bash/install-oh-my-zsh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update package list
+apt-get update
+apt-get -y upgrade
+
+# Install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
+printf "\nsource ~/.bash_aliases\n" | tee -a /home/vagrant/.zshrc
+printf "\nsource ~/.profile\n" | tee -a /home/vagrant/.zshrc
+chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
+chown vagrant:vagrant /home/vagrant/.zshrc
